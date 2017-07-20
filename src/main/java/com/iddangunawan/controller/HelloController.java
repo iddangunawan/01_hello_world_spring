7f93c984bdd8209c7f0e0de38b07c3cbe01cca1c
Iddan Gunawan <iddan_gunawan@yahoo.co.id>
2017-07-20 09:56:39 +0700

    hello world

---
+++ src/main/java/com/iddangunawan/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.iddangunawan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	@RequestMapping("hi")
+	@ResponseBody
+	public String hi() {
+		return "Bismillah <br> Hallow Berayyy!!!";
+	}
+}
